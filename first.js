86b4b93dab68fa4d45fec4796d75d3fda6a70037
dipongkor000 <dipongkorroy000@gmail.com>
2025-03-02 01:41:53 +0600

    add some text

--- first.js
+++ first.js
@@ -1,25 +1,25 @@
 let allBtn = document.querySelectorAll('.btn');
 for (let btn of allBtn) {
-    btn.addEventListener('click', function () {
-
+    btn.addEventListener('click', function (even) {
+        
     let totalNum = document.getElementById('total-number').innerText;
     let convertTotalNum = parseInt(totalNum);
     let checkboxNum = document.getElementById('checkbox-number').innerText;
         let convertCheckboxNum = parseInt(checkboxNum);
-        
+        btn.disable = true;
+
         if (convertTotalNum > 1) {
             btn.classList.remove('bg-blue-500')
-            btn.classList.add('bg-gray-400')
-    
+            btn.classList.add('bg-gray-400');
+            
             let sub = convertTotalNum - 1;
             document.getElementById('total-number').innerText = sub;
     
             let sum = convertCheckboxNum + 1;
             document.getElementById('checkbox-number').innerText = sum;
             
-
-            alert("Well, you have completed this task.")
-        } else{
+            alert("Board updated Successfully.")
+        } else if(convertTotalNum >=1){
             btn.classList.remove('bg-blue-500')
             btn.classList.add('bg-gray-400')
             
@@ -28,9 +28,59 @@ for (let btn of allBtn) {
             
             let sum = convertCheckboxNum + 1;
             document.getElementById('checkbox-number').innerText = sum;
-            alert("Congratulations, you've finished all the tasks.")
+            alert("Congrats! you've finished all the tasks.")
+            stop;
         }
+        const title = even.target.parentNode.parentNode;
+        const h1Title = title.querySelector('.div-title').innerText;
+        const divHistory = document.getElementById('history-div');
+
+        const history = document.createElement('div');
+        history.innerHTML = `<p class = "bg-blue-50 rounded-md p-2 mb-3"> You have completed the task ${h1Title} at </p> <span id = 'time' class = 'time'></span>`;
+
+        divHistory.appendChild(history);
+        
     })
 }
+
+
+function page() {
+    window.location.href = 'index2.html'
+}
+
+
+
+let colors = ['red','blue','yellow','purple','green','orange','tomato','sky','pink']
+let bgColor = document.getElementById('color-btn');
+bgColor.addEventListener('click', function () {
+    let i = 0;
+    if ( i < colors.length){
+        document.getElementById('body-bg').style.backgroundColor = colors[i];
+        i = i + i;
+        
+    }
+})
+
+
+function displayDate() {
+    const now = new Date();
+
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    const date = now.toLocaleDateString('en-BD', options);
+
+    document.getElementById('date').textContent = date;
+}
+displayDate();
+
+const clearBtn = document.getElementById('clear-btn');
+clearBtn.addEventListener('click', function () {
+    document.getElementById('history-div').textContent = '';
+})
+
 
 
